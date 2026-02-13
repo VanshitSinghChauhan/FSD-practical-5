5ddac159ca87174ad0b4de9eed1ba7dc62e370a6
Vanshit Singh Chauhan <vanshitsinghchauhan16032005@gmail.com>
2026-02-13 22:54:51 +0530

    Uploading everything including node modules

---
+++ routes/cartRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+let cart = []; 
+
+router.get('/', (req, res) => {
+    res.json({ message: "Your cart items", data: cart });
+});
+
+router.post('/', (req, res) => {
+    const { productId, quantity } = req.body;
+
+    if (!productId || !quantity) {
+        return res.status(400).json({ message: "Missing product ID or quantity" });
+    }
+
+    cart.push({ productId, quantity });
+    res.status(201).json({ message: "Added to cart" });
+});
+
+module.exports = router;
